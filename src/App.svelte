b5b2ed27264bc29fa18e4f95998cbb0502f46667
urzk <shgerk@gmail.com>
2024-10-19 19:16:40 +0900

    made views

--- src/App.svelte
+++ src/App.svelte
@@ -1,49 +1,38 @@
-<script lang="ts">
-  import svelteLogo from "./assets/svelte.svg";
-  import viteLogo from "/vite.svg";
-  import Counter from "./lib/Counter.svelte";
+<script>
+  import { onMount } from "svelte";
+  import ItemTab from "./components/ItemTab.svelte";
+
+  let tabs = [];
+
+  onMount(() => {
+    // Chrome タブ一覧を取得
+    chrome.tabs.query({ currentWindow: true }, function (result) {
+      tabs = result;
+    });
+  });
 </script>
 
 <main>
-  <div>
-    <a href="https://vitejs.dev" target="_blank" rel="noreferrer">
-      <img src={viteLogo} class="logo" alt="Vite Logo" />
-    </a>
-    <a href="https://svelte.dev" target="_blank" rel="noreferrer">
-      <img src={svelteLogo} class="logo svelte" alt="Svelte Logo" />
-    </a>
+  <div class="container-tabs">
+    <h2>Open Tabs</h2>
+    <ul>
+      {#each tabs as tab}
+        <ItemTab title={tab.title} url={tab.url} />
+      {/each}
+    </ul>
   </div>
-  <h1>ねこちゃん。</h1>
-
-  <div class="card">
-    <Counter />
-  </div>
-
-  <p>
-    Check out <a
-      href="https://github.com/sveltejs/kit#readme"
-      target="_blank"
-      rel="noreferrer">SvelteKit</a
-    >, the official Svelte app framework powered by Vite!
-  </p>
-
-  <p class="read-the-docs">Click on the Vite and Svelte logos to learn more</p>
 </main>
 
 <style>
-  .logo {
-    height: 6em;
-    padding: 1.5em;
-    will-change: filter;
-    transition: filter 300ms;
+  .container-tabs {
+    padding: 4rem;
   }
-  .logo:hover {
-    filter: drop-shadow(0 0 2em #646cffaa);
-  }
-  .logo.svelte:hover {
-    filter: drop-shadow(0 0 2em #ff3e00aa);
-  }
-  .read-the-docs {
-    color: #888;
+  .container-tabs ul {
+    display: flex;
+    flex-wrap: wrap;
+    gap: 1rem;
+    justify-content: space-between;
+    margin: 0;
+    padding: 0;
   }
 </style>
---
+++ src/components/ItemTab.svelte
@@ -0,0 +1,51 @@
+<script lang="ts">
+  export let title: string;
+  export let url: string;
+  //  export let thumb: string;
+</script>
+
+<li>
+  <div class="item-tab-thumb"></div>
+  <div class="item-tab-text">
+    <h3 class="item-tab-title">{title}</h3>
+    <p class="item-tab-url">{url}</p>
+  </div>
+</li>
+
+<style>
+  li {
+    display: flex;
+    justify-content: space-between;
+    margin: 0;
+    padding: 1rem;
+    width: 300px;
+    background-color: aliceblue;
+    border-radius: 0.25rem;
+    color: black;
+  }
+  li .item-tab-thumb {
+    width: 25%;
+    height: 32px;
+    background-color: aqua;
+  }
+  li .item-tab-text {
+    width: 70%;
+  }
+  .item-tab-title {
+    margin-top: 0;
+    margin-bottom: 0.5rem;
+    display: -webkit-box;
+    -webkit-box-orient: vertical;
+    -webkit-line-clamp: 2;
+    overflow: hidden;
+    text-overflow: ellipsis;
+  }
+  .item-tab-url {
+    margin: 0;
+    display: -webkit-box;
+    -webkit-box-orient: vertical;
+    -webkit-line-clamp: 1;
+    overflow: hidden;
+    text-overflow: ellipsis;
+  }
+</style>
